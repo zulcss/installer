b16cb1b2af6dd721f05d86ef86a031a296dbc5aa
Charles Short <charles.short@windriver.com>
2024-03-09 16:56:46 +0000

    add parted
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- tiler.py
+++ tiler.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python3
 
+import argparse
+import sys
+
+from tiler.utils import run_command
+
+def create_disk(disk):
+    print(f"Setting up partitions on {disk}")
+
+    run_command(["parted", "-s", disk, "mklabel", "gpt"])
+    run_command(
+        ["parted", "-s", disk, "mkpart", "EFI", "FAT32", "1MB", "500MB"])
+    run_command(
+        ["parted", "-s", disk, "set", "1", "esp", "on"])
+    run_command(
+        ["parted", "-s", disk, "set", "1", "boot", "on"])
+    run_command(
+        ["parted", "-s", disk, "mkpart", "ROOT", "ext4", "500Mb", "100%"])
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Debian automated installer")
+    parser.add_argument("--device", help="Target device")
+
+    return parser.parse_args()
+
 def main():
-    print("hello world")
+    args = parse_args()
+
+    if args.device is None:
+        print("Device is not specified.")
+        sys.exit(1)
+
+    print(f"Installing Debian onto {args.device}.")
+    try:
+        create_disk(args.device)
+    finally:
+        print(f"Succesfully installed Debian on {args.device}")
 
 if __name__ == '__main__':
     main()
--- tiler/utils.py
+++ tiler/utils.py
@@ -12,14 +12,14 @@ def run_command(args, data=None, env=None, capture=False, shell=False):
         stdin = subprocess.PIPE
         sp = subprocess.Popen(args, stdout=stdout,
                               stderr=stderr, stdin=stdin,
-                              env=env, shell=shell)
+                              env=env, shell=shell, universal_newlines=True)
         (out, err) = sp.communicate(data)
     except OSError as e:
         raise Exception(f"failed to run cmd: {args}")
     # Just ensure blank instead of none
     if not out and capture:
-       out = ""
+       out = out
     if not err and capture:
-       err = ""
+       err = err
     return (out, err)
                               
