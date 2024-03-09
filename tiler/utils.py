36db10c64de06ea7988abfeab59302be93292d26
Charles Short <charles.short@windriver.com>
2024-03-09 15:20:05 +0000

    Add run_command
    Signed-off-by: Charles Short <charles.short@windriver.com>

---
+++ tiler/utils.py
@@ -0,0 +1,25 @@
+import subprocess
+
+
+def run_command(args, data=None, env=None, capture=False, shell=False):
+    try:
+        if not capture:
+            stdout = None
+            stderr = None
+        else:
+            stdout = subprocess.PIPE
+            stderr = subprocess.PIPE
+        stdin = subprocess.PIPE
+        sp = subprocess.Popen(args, stdout=stdout,
+                              stderr=stderr, stdin=stdin,
+                              env=env, shell=shell)
+        (out, err) = sp.communicate(data)
+    except OSError as e:
+        raise Exception(f"failed to run cmd: {args}")
+    # Just ensure blank instead of none
+    if not out and capture:
+       out = ""
+    if not err and capture:
+       err = ""
+    return (out, err)
+                              
