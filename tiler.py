bb493b56082ae391379e131bcc6d58915c242921
Charles Short <charles.short@windriver.com>
2024-03-10 22:40:34 +0000

    install bookworm
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- tiler.py
+++ tiler.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
 import argparse
+import os
+import pathlib
+import shutil
 import sys
 
-from tiler.utils import run_command
+from tiler.utils import (
+    run_command,
+    run_chroot
+)
 
 def create_disk(disk):
     print(f"Setting up partitions on {disk}")
@@ -18,9 +24,70 @@ def create_disk(disk):
     run_command(
         ["parted", "-s", disk, "mkpart", "ROOT", "ext4", "500Mb", "100%"])
 
+def create_filesystem(disk):
+    print("Crweating filesystems")
+
+    print("Formatting EFI filesytem.")
+    run_command(
+        ["mkfs.vfat", "-F32", "-n", "EFI",  f"{disk}p1"])
+    run_command(
+        ["mkfs", "-t", "ext4", "-L", "ROOT", f"{disk}p2"])
+
+def create_workspace():
+    return pathlib.Path("/var/tmp/tiler")
+
+def extract_archive(disk, tarball):
+    print("Extracting archve")
+
+    workspace = create_workspace()
+    rootfs = workspace.joinpath("rootfs")
+    if rootfs.exists():
+        shutil.rmtree(rootfs)
+    rootfs.mkdir(parents=True)
+
+    print("Mounting file system")
+    run_command(
+        ["mount", f"{disk}p2", rootfs])
+
+    efi = rootfs.joinpath("efi")
+    run_command(
+        ["tar", "-C", rootfs, "-zxf", tarball, "--numeric-owner"])
+    efi.mkdir(parents=True, exist_ok=True)
+
+    run_command(["umount", rootfs])
+    return rootfs
+
+def install_bootloader(disk):
+    workspace = create_workspace()
+    rootfs = workspace.joinpath("rootfs")
+    efi = rootfs.joinpath("efi")
+
+    print("Installting boot loader.")
+    run_command(
+        ["mount", f"{disk}p2", rootfs])
+    run_command(
+        ["mount", f"{disk}p1", efi])
+
+    print("Configuring kernel")
+    kernel_conf = rootfs.joinpath("etc/kernel/cmdline")
+    with open(kernel_conf, "w") as f:
+        f.write(
+            "root=LABEL=ROOT rw console=tty0 console=ttyS0,115200n8 rootwait rw")
+    run_chroot(
+       ["bootctl", "install", "--no-variables",  "--entry-token", "os-id"], rootfs)
+    run_chroot(
+        ["kernel-install", "add","6.1.0-18-amd64", "/boot/vmlinuz-6.1.0-18-amd64"], rootfs)
+
+    run_command(
+        ["umount", f"{disk}p1", efi])
+    run_command(
+        ["umount", f"{disk}p2", rootfs])
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Debian automated installer")
     parser.add_argument("--device", help="Target device")
+    parser.add_argument("--tarball", help="Path to the tarball.")
 
     return parser.parse_args()
 
@@ -31,9 +98,16 @@ def main():
         print("Device is not specified.")
         sys.exit(1)
 
+    if args.tarball is None:
+        print("Tarball is not specified")
+        sys.exit(1)
+
     print(f"Installing Debian onto {args.device}.")
     try:
         create_disk(args.device)
+        create_filesystem(args.device)
+        extract_archive(args.device, args.tarball)
+        install_bootloader(args.device)
     finally:
         print(f"Succesfully installed Debian on {args.device}")
 
--- tiler/utils.py
+++ tiler/utils.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 
@@ -22,4 +23,33 @@ def run_command(args, data=None, env=None, capture=False, shell=False):
     if not err and capture:
        err = err
     return (out, err)
-                              
+
+def run_chroot(args, rootfs, data=None, env=None, capture=False, shell=False):
+    try:
+        cmd = [
+             "bwrap",
+             "--bind", rootfs, "/",
+             "--bind", f"{rootfs}/efi" , "/efi",
+             "--bind", "/sys/firmware/efi/efivars", "/sys/firmware/efi/efivars",
+             "--proc", "/proc",
+             "--dev-bind", "/dev", "/dev",
+             "--dir", "/run",
+             "--bind", "/tmp", "/tmp",
+             "--bind", "/sys", "/sys",
+             "--share-net",
+             "--die-with-parent",
+             "--chdir", "/",
+        ]
+        cmd += args
+        (out, err) = run_command(cmd,
+                    data=data,
+                    env=env,
+                    capture=capture,
+                    shell=shell)
+    except OSError as e:
+        raise Exception(f"Failed to run chroot command: {args}")
+    if not out and capture:
+        out = out
+    if not err and capture:
+        err = err
+    return (out, err)
